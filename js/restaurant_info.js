c6495b08685c988d9feb5cdcdf49d569eaf65f04
Davis <davis.sermons@gmail.com>
2018-07-09 13:40:20 +0300

    style:css styling for main page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiZGF2aXM2MjYiLCJhIjoiY2pqYmxod3V0MDRqbzNwcWxhcGcyMDB0YSJ9.IQZ62Kjh3g_H_dWIsGEOfA',
+        mapboxToken: 'pk.eyJ1IjoiZGF2aXM2MjYiLCJhIjoiY2pqY3Q4dHVnMjh5YzN3cGw3djg4a2E2ZCJ9.X7OU2tMA_7Dy6evE_eLjpw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
