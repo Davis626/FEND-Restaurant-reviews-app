934c66cb33fd63d7f82f6eb977f045489ec9cd40
Davis <davis.sermons@gmail.com>
2018-07-09 22:24:14 +0300

    refactor:service worker js file

--- js/sw.js
+++ js/sw.js
@@ -52,9 +52,10 @@ self.addEventListener('activate', event => {
     );
 });
 
-self.addEventListener('fetch', function (event) {
+// fetch data
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
